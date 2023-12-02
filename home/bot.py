5d2009b6c1318e3b47c1b3b26feebc436c924a02
riken-khadela <rikenkhadelamain@gmail.com>
2023-12-02 16:22:38 +0530

    sasasa

--- home/bot.py
+++ home/bot.py
@@ -63,6 +63,7 @@ class Bot():
         options.add_argument('--no-sandbox')
         options.add_argument('--start-maximized')    
         options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--disable-gpu')
         options.add_argument("--ignore-certificate-errors")
         options.add_argument("--enable-javascript")
         options.add_argument("--enable-popup-blocking")
