1d00e345e0aa9bcc7304b801f4152574fca23889
riken-khadela <rikenkhadelamain@gmail.com>
2023-12-02 23:37:56 +0530

    kk

--- home/bot.py
+++ home/bot.py
@@ -59,7 +59,7 @@ class Bot():
         
         for _ in range(30):
             try:
-                self.driver = uc(user_data_dir='profile/'+str(profile_id),headless=True)
+                self.driver = uc(user_data_dir='profile/'+str(profile_id))
                 break
             except Exception as e:
                 print(e)
