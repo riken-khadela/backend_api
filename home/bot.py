3d891d4aec78bbcb8added2deea45b51ddb9f6cb
riken-khadela <rikenkhadelamain@gmail.com>
2023-12-03 00:18:48 +0530

    remove port

--- home/bot.py
+++ home/bot.py
@@ -57,7 +57,7 @@ class Bot():
         # options.add_experimental_option("prefs", prefs)
         
         
-        self.driver = uc(user_data_dir=str(profile_id),headless=True,version_main=119,port=2222)
+        self.driver = uc(user_data_dir=str(profile_id),headless=True,version_main=119)
         return self.driver
     def find_element(self, element, locator, locator_type=By.XPATH,
             page=None, timeout=10,
