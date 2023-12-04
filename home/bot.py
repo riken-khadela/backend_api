86f045430ab936b5fc209fc8984be4b2ff987b84
riken-khadela <rikenkhadelamain@gmail.com>
2023-12-04 13:44:49 +0530

    sss

--- home/bot.py
+++ home/bot.py
@@ -26,7 +26,7 @@ class Bot():
         from selenium.webdriver.chrome.options import Options
         options = webdriver.ChromeOptions()
         service = Service(chromedriver_path)
-        service.enable_log()
+        # service.enable_log()
         options.binary_location = chrome_binary_path
         options.add_argument('--headless')  
         options.add_argument('--no-sandbox')  
