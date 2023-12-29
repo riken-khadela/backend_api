775b12fcb11243e7011d8c9a82f883e278ac7bcd
riken-khadela <rikenkhadelamain@gmail.com>
2023-12-29 17:27:52 +0530

    added port in cores

--- e_com/settings.py
+++ e_com/settings.py
@@ -192,6 +192,7 @@ PASSWORD_RESET_TIMEOUT=900          # 900 Sec = 15 Min
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "*"
 ]
 
 STATIC_URL = '/static/'
