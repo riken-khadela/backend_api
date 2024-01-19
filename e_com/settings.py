70f8779e9c3d950048ed163fef88b825f63019ae
riken-khadela <rikenkhadelamain@gmail.com>
2024-01-20 04:28:46 +0530

    added CORS_ALLOW_CREDENTIALS = True

--- e_com/settings.py
+++ e_com/settings.py
@@ -51,13 +51,11 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
     # 'home.middleware.TokenAuthenticationMiddleware'
 ]
 
@@ -193,6 +191,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "*"
 ]
+CORS_ALLOW_CREDENTIALS = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
